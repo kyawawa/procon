3395507c8e27193ff8da21c52103fdf5e45cb3d8
Tatsuya Ishikawa <ishikawa@jsk.imi.i.u-tokyo.ac.jp>
2018-11-30 12:26:12 +0900

    [atcoder/AGC] Add agc028 (only a.cpp)

---
+++ atcoder/AGC/agc028/src/b.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+using lli = long long int;
+
+int main()
+{
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    const lli MOD = 1000000000 + 7;
+
+    int N;
+    cin >> N;
+
+    vector<lli> a(N);
+    for (int i = 0; i < N; ++i) {
+        cin >> a[i];
+    }
+
+
+
+    return 0;
+}
