9e36be8ff7c4ba6c88ef9efc2ccbabd2d23a3e97
Tatsuya Ishikawa <ishikawa@jsk.imi.i.u-tokyo.ac.jp>
2019-04-27 23:08:46 +0900

    [abc125] Fix C (with official explanation)

--- atcoder/ABC/abc125/src/c.cpp
+++ atcoder/ABC/abc125/src/c.cpp
@@ -23,40 +23,6 @@ T gcd(T a, T b)
     return ret;
 }
 
-// fail1
-// 1個抜きのGCDをN個求め、その最大値が答え
-// 時間が間に合わない
-// int main()
-// {
-//     cin.tie(0);
-//     ios::sync_with_stdio(false);
-
-//     int N;
-//     cin >> N;
-//     vector<int> A(N);
-//     for (int i = 0; i < N; ++i) cin >> A[i];
-
-//     int max_gcd = 1;
-//     for (int i = 0; i < N; ++i) {
-//         int tmp_gcd = 0;
-//         for (int j = 0; j < N; ++j) {
-//             if (i == j) continue;
-//             if (tmp_gcd == 0) {
-//                 tmp_gcd = A[j];
-//                 continue;
-//             }
-//             tmp_gcd = gcd(tmp_gcd, A[j]);
-//         }
-//         max_gcd = max(max_gcd, tmp_gcd);
-//     }
-
-//     cout << max_gcd << endl;
-
-//     return 0;
-// }
-
-// fail2
-// 明らかに間違い
 int main()
 {
     cin.tie(0);
@@ -67,18 +33,25 @@ int main()
     vector<int> A(N);
     for (int i = 0; i < N; ++i) cin >> A[i];
 
-    vector<int> gcd_list(N);
-    for (int i = 0; i < N - 1; ++i) {
-        gcd_list[i] = gcd(A[i], A[i+1]);
-    }
-    gcd_list[N-1] = gcd(A[N-1], A[0]);
-    sort(gcd_list.begin(), gcd_list.end(), greater<int>());
+    // 左から1つずつ要素を増やすgcdと、
+    // 右から1つずつ要素を減らすgcdをリスト保持
+    // 特に、右からのやつをリスト保持しないと計算量が大変なことになる
+    // gcdは要素を足せるが減らせない
 
-    int max_gcd = gcd_list[0];
-    for (int i = 1; i < N - 2; ++i) { // ボトルネックを含むgcdは2つ
-        max_gcd = gcd(max_gcd, gcd_list[i]);
+    vector<int> left_gcd(N);
+    vector<int> right_gcd(N);
+    left_gcd[0] = 0;
+    right_gcd[N-1] = 0;
+
+    for (int i = 1; i < N; ++i) {
+        left_gcd[i] = gcd(left_gcd[i-1], A[i-1]);
+        right_gcd[N-i-1] = gcd(right_gcd[N-i], A[N-i]);
     }
 
+    int max_gcd = 1;
+    for (int i = 0; i < N; ++i) {
+        max_gcd = max(max_gcd, gcd(left_gcd[i], right_gcd[i]));
+    }
     cout << max_gcd << endl;
 
     return 0;
